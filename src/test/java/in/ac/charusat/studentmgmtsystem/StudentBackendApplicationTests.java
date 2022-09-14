26ad7bace57b183bc91b8f0ef551da5fecfa0186
20it135 <80442908+shailshah2757@users.noreply.github.com>
2022-09-14 21:44:17 +0530

    Initial commit

---
+++ src/test/java/in/ac/charusat/studentmgmtsystem/StudentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ac.charusat.studentmgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
